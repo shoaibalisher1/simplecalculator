2556fadd801a4110401d62b3c529f0ba638dfb57
shoaibalisher1 <166815957+shoaibalisher1@users.noreply.github.com>
2024-04-13 16:53:33 +0500

    Sample Calculator

---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+// SHABANG
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstnumber" },
+  { message: "Enter second number", type: "number", name: "secondnumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "Operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// console.log(answer)
+// Conditional Statement
+if (answer.Operator === "Addition") {
+console.log(answer.firstnumber + answer.secondnumber);
+}
+
+ else if (answer.Operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+ else if (answer.Operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+ else if (answer.Operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+} else {
+    console.log ("Please enter valid operator")
+}
